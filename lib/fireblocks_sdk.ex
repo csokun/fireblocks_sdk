392d3058a5a588ff9eeef522719da8094ef04466
Sokun Chorn (The Blockchain Guy) <chornsokun@gmail.com>
2023-03-14 23:43:26 +1100

    estimate network fee

--- lib/fireblocks_sdk.ex
+++ lib/fireblocks_sdk.ex
@@ -10,18 +10,17 @@ defmodule FireblocksSdk do
   Gets all assets that are currently supported by Fireblocks
   """
   @spec get_supported_assets() :: [Models.asset_type_response()]
-  def get_supported_assets() do
-    get("/v1/supported_assets")
-  end
+  def get_supported_assets(), do: get("/v1/supported_assets")
 
   @doc """
   List all users for the workspace.
 
   Please note that this endpoint is available only for API keys with Admin permissions.
   """
-  def get_users() do
-    get!("/v1/users")
-  end
+  def get_users(), do: get!("/v1/users")
+
+  def estimate_network_fee(asset_id) when is_binary(asset_id),
+    do: get!("/v1/estimate_network_fee?assetId=#{asset_id}")
 
   @doc """
   Get audit logs by day.
