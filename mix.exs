72537e91a283daeecd55eab9099163844bbba16d
Sokun Chorn (The Blockchain Guy) <chornsokun@gmail.com>
2023-05-23 15:42:51 +1000

    remove application

--- mix.exs
+++ mix.exs
@@ -25,8 +25,7 @@ defmodule FireblocksSdk.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :crypto],
-      mod: {FireblocksSdk.Application, []}
+      extra_applications: [:logger, :crypto]
     ]
   end
 
