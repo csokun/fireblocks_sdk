df174a69030e031c81ecfb58eab886155d4a031f
sokun <chornsokun@gmail.com>
2025-07-09 14:30:34 +1000

    Fix schema option key from require to required in asset API

--- lib/fireblocks_sdk/api/asset.ex
+++ lib/fireblocks_sdk/api/asset.ex
@@ -4,15 +4,15 @@ defmodule FireblocksSdk.Api.Asset do
   @assets_path "/v1/assets"
 
   @register_asset_schema [
-    blockchainId: [type: :string, require: true],
-    address: [type: :string, require: true],
-    symbol: [type: :string, require: true]
+    blockchainId: [type: :string, required: true],
+    address: [type: :string, required: true],
+    symbol: [type: :string, required: true]
   ]
 
   @update_asset_price_schema [
-    assetId: [type: :string, require: true],
-    currency: [type: :string, require: true],
-    price: [type: {:or, [:integer, :float]}, require: true]
+    assetId: [type: :string, required: true],
+    currency: [type: :string, required: true],
+    price: [type: {:or, [:integer, :float]}, required: true]
   ]
 
   @doc """
@@ -44,8 +44,8 @@ defmodule FireblocksSdk.Api.Asset do
   Set asset price for the given asset id. Returns the asset price response.
 
   ```
-    FireblocksSdk.Api.Asset.register([
-      assetId: "ETH_TEST3",
+    FireblocksSdk.Api.Asset.set_price([
+      assetId: "USD1_B75VRLGX_TQL4",
       currency: "USD",
       price: 1000
     ])
@@ -53,14 +53,16 @@ defmodule FireblocksSdk.Api.Asset do
 
   Options: \n#{NimbleOptions.docs(@update_asset_price_schema)}
   """
-  def set_price(price, idempotent_key \\ "") do
-    {:ok, options} = NimbleOptions.validate(price, @update_asset_price_schema)
+  def set_price(opts, idempotent_key \\ "") do
+    {:ok, options} = NimbleOptions.validate(opts, @update_asset_price_schema)
+    assetId = Keyword.get(opts, :assetId)
 
     params =
       options
       |> Keyword.delete(:assetId)
+      |> Enum.into(%{})
       |> Jason.encode!()
 
-    post!("#{@assets_path}/prices/#{params[:assetId]}", params, idempotent_key)
+    post!("#{@assets_path}/prices/#{assetId}", params, idempotent_key)
   end
 end
