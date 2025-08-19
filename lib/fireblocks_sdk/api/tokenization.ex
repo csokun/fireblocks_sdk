469d20eddd74a5ab2c08a289148a6c96402b2379
sokun <chornsokun@gmail.com>
2025-08-20 09:57:11 +1000

    Add create/2 function for tokenization API

--- lib/fireblocks_sdk/api/tokenization.ex
+++ lib/fireblocks_sdk/api/tokenization.ex
@@ -7,6 +7,8 @@ defmodule FireblocksSdk.Api.Tokenization do
 
   @doc """
   Return all linked tokens (paginated)
+
+  Options:\n#{NimbleOptions.docs(Schema.tokenization_list_request())}
   """
   def list(filter) do
     {:ok, params} = NimbleOptions.validate(filter, Schema.tokenization_list_request())
@@ -19,5 +21,18 @@ defmodule FireblocksSdk.Api.Tokenization do
   """
   def get(token_id) do
     get!("#{@base_path}/tokens/#{token_id}")
+  end
+
+  @doc """
+  Facilitates the creation of a new token, supporting both EVM-based and Stellar/Ripple platforms.
+  For EVM, it deploys the corresponding contract template to the blockchain and links the token to the workspace.
+  For Stellar/Ripple, it links a newly created token directly to the workspace without deploying a contract.
+  Returns the token link with status "PENDING" until the token is deployed or "SUCCESS" if no deployment is needed.
+
+  Options:\n#{NimbleOptions.docs(Schema.tokenization_create_request())}
+  """
+  def create(params, idempotentKey \\ "") do
+    {:ok, params} = NimbleOptions.validate(params, Schema.tokenization_create_request())
+    post!("#{@base_path}/tokens", params, idempotentKey)
   end
 end
