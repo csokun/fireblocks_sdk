8a0659484e4c897985a1a88824f550a9cf12b20f
sokun <chornsokun@gmail.com>
2025-11-14 18:27:23 +1100

    webhook v2

--- lib/fireblocks_sdk/api/webhook.ex
+++ lib/fireblocks_sdk/api/webhook.ex
@@ -123,7 +123,7 @@ defmodule FireblocksSdk.Api.Webhook do
 
   ```
   # Get notifications for a specific webhook
-  FireblocksSdk.Api.Webhook.notifications([
+  FireblocksSdk.Api.Webhook.get_notifications([
     webhookId: "wh_123abc",
     order: :desc,
     events: ["transaction.created", "embedded_wallet.asset.added"],
@@ -134,7 +134,7 @@ defmodule FireblocksSdk.Api.Webhook do
 
   Options:\n#{NimbleOptions.docs(@webhook_v2_notifications_request)}
   """
-  def notifications(webhook) do
+  def get_notifications(webhook) do
     {:ok, options} = NimbleOptions.validate(webhook, @webhook_v2_notifications_request)
 
     webhook_id = options[:webhookId]
@@ -147,5 +147,188 @@ defmodule FireblocksSdk.Api.Webhook do
       |> URI.encode_query()
 
     get!("#{@base_path}/#{webhook_id}?#{query_string}")
+  end
+
+  @webhook_v2_get_notification_request [
+    includeData: [
+      type: :boolean,
+      default: false,
+      doc: "Whether to include notification data in the response."
+    ],
+    webhookId: [type: :string, required: true, doc: "The ID of the webhook to fetch"],
+    notificationId: [type: :string, required: true, doc: "The ID of the notification to fetch"]
+  ]
+
+  @doc """
+  Retrieve a specific notification by its id.
+
+  Options:\n#{NimbleOptions.docs(@webhook_v2_get_notification_request)}
+  """
+  def get_notification(notification \\ []) do
+    {:ok, options} = NimbleOptions.validate(notification, @webhook_v2_get_notification_request)
+
+    webhook_id = options[:webhookId]
+    notification_id = options[:notificationId]
+
+    query_string =
+      options
+      |> Keyword.delete(:webhookId)
+      |> Keyword.delete(:notificationId)
+      |> URI.encode_query()
+
+    get!("#{@base_path}/#{webhook_id}/notifications/#{notification_id}?#{query_string}")
+  end
+
+  @webhook_v2_get_notification_attempts_request [
+    webhookId: [type: :string, required: true, doc: "The ID of the webhook to fetch"],
+    notificationId: [type: :string, required: true, doc: "The ID of the notification to fetch"],
+    pageCursor: [type: :string],
+    pageSize: [type: :integer, default: 10]
+  ]
+
+  @doc """
+  Get notification attempts by notification id
+
+  Options:\n#{NimbleOptions.docs(@webhook_v2_get_notification_attempts_request)}
+  """
+  def get_notification_attempts(notification \\ []) do
+    {:ok, options} =
+      NimbleOptions.validate(notification, @webhook_v2_get_notification_attempts_request)
+
+    webhook_id = options[:webhookId]
+    notification_id = options[:notificationId]
+
+    query_string =
+      options
+      |> Keyword.delete(:webhookId)
+      |> Keyword.delete(:notificationId)
+      |> URI.encode_query()
+
+    get!("#{@base_path}/#{webhook_id}/notifications/#{notification_id}/attempts?#{query_string}")
+  end
+
+  @webhook_v2_resend_notification_request [
+    webhookId: [
+      type: :string,
+      required: true,
+      doc: "The ID of the webhook to resend notification for"
+    ],
+    notificationId: [type: :string, required: true, doc: "The ID of the notification to resend"]
+  ]
+
+  @doc """
+  Resend a specific notification by its id.
+
+  **Endpoint Permission:** Owner, Admin, Non-Signing Admin, Editor, Signer.
+
+  Options:\n#{NimbleOptions.docs(@webhook_v2_resend_notification_request)}
+  """
+  def resend(notification \\ [], idempotentKey \\ "") do
+    {:ok, options} = NimbleOptions.validate(notification, @webhook_v2_resend_notification_request)
+
+    webhook_id = options[:webhookId]
+    notification_id = options[:notificationId]
+
+    post!(
+      "#{@base_path}/#{webhook_id}/notifications/#{notification_id}/resend",
+      "",
+      idempotentKey
+    )
+  end
+
+  @webhook_v2_resend_by_resource_request [
+    webhookId: [
+      type: :string,
+      required: true,
+      doc: "The ID of the webhook to resend notifications for"
+    ],
+    resourceId: [
+      type: :string,
+      required: true,
+      doc: "The resource ID to resend notifications for"
+    ],
+    excludeStatuses: [
+      type: {:list, {:in, [:completed, :failed, :in_progress, :on_hold]}},
+      doc:
+        "Filter notifications by status. Available values: `:completed`, `:failed`, `:in_progress`, `:on_hold`."
+    ]
+  ]
+
+  @doc """
+  Resend notifications by resource ID.
+
+  **Endpoint Permission:** Owner, Admin, Non-Signing Admin, Editor, Signer.
+
+  Options:\n#{NimbleOptions.docs(@webhook_v2_resend_by_resource_request)}
+  """
+  def resend_by_resource(notification \\ [], idempotentKey \\ "") do
+    {:ok, options} = NimbleOptions.validate(notification, @webhook_v2_resend_by_resource_request)
+
+    webhook_id = options[:webhookId]
+    resource_id = options[:resourceId]
+
+    params =
+      options
+      |> Keyword.delete(:webhookId)
+      |> atom_to_upper([:excludeStatuses])
+      |> Jason.encode!()
+
+    post!(
+      "#{@base_path}/#{webhook_id}/notifications/resend_by_resource",
+      params,
+      idempotentKey
+    )
+  end
+
+  @webhook_v2_resend_failed_request [
+    webhookId: [
+      type: :string,
+      required: true,
+      doc: "The ID of the webhook to resend failed notifications for"
+    ],
+    startTime: [
+      type: :integer,
+      doc:
+        "Start time in milliseconds since epoch to filter by notifications created after this time (default 31 days ago)"
+    ],
+    events: [
+      type: {:list, :string},
+      doc: """
+      Available values : `transaction.created`, `transaction.status.updated`, `transaction.approval_status.updated`, `transaction.network_records.processing_completed`, `external_wallet.asset.added`, `external_wallet.asset.removed`, `internal_wallet.asset.added`, `internal_wallet.asset.removed`, `contract_wallet.asset.added`, `contract_wallet.asset.removed`, `vault_account.created`, `vault_account.asset.added`, `vault_account.asset.balance_updated`, `embedded_wallet.status.updated`, `embedded_wallet.created`, `embedded_wallet.asset.balance_updated`, `embedded_wallet.asset.added`, `embedded_wallet.account.created`, `embedded_wallet.device.added`, `onchain_data.updated`, `connection.added`, `connection.removed`, `connection.request.waiting_peer_approval`, `connection.request.rejected_by_peer`
+      """
+    ]
+  ]
+
+  @doc """
+  Resend all failed notifications for a webhook, optionally filtered by resource ID.
+
+  **Endpoint Permission:** Owner, Admin, Non-Signing Admin, Editor, Signer.
+
+  Options:\n#{NimbleOptions.docs(@webhook_v2_resend_failed_request)}
+  """
+  def resend_failed(notification \\ [], idempotentKey \\ "") do
+    {:ok, options} = NimbleOptions.validate(notification, @webhook_v2_resend_failed_request)
+
+    webhook_id = options[:webhookId]
+
+    params =
+      options
+      |> Keyword.delete(:webhookId)
+      |> Jason.encode!()
+
+    post!(
+      "#{@base_path}/#{webhook_id}/notifications/resend_failed",
+      params,
+      idempotentKey
+    )
+  end
+
+  @doc """
+  Get the status of a resend job
+  """
+  def get_resend_job_status(webhook_id, job_id)
+      when is_binary(webhook_id)
+      when is_binary(job_id) do
+    get!("#{base_path}/#{webhook_id}/notifications/resend_failed/jobs/#{job_id}")
   end
 end
