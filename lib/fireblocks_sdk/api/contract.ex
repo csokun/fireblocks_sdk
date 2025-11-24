911e6816defebe81d74a4257de5562e4516f894f
sokun <chornsokun@gmail.com>
2025-11-24 16:28:54 +1100

    Add contract interaction API functions

--- lib/fireblocks_sdk/api/contract.ex
+++ lib/fireblocks_sdk/api/contract.ex
@@ -2,6 +2,7 @@ defmodule FireblocksSdk.Api.Contract do
   import FireblocksSdk.Request
 
   @contract_endpoint "/v1/contracts"
+  @interactions "/interactionss/baseAssetId"
 
   @add_asset_schema [
     contractId: [type: :string, require: true],
@@ -78,5 +79,160 @@ defmodule FireblocksSdk.Api.Contract do
   """
   def delete_asset(contract_id, asset_id) do
     delete!("#{@contract_endpoint}/#{contract_id}/assets/#{asset_id}")
+  end
+
+  @interaction_functions [
+    baseAssetId: [type: :string, required: true, doc: "Base assetId e.g ETH, ETH_TEST5"],
+    contractAddress: [type: :string, required: true]
+  ]
+
+  @doc """
+  Return deployed contract's ABI by blockchain native asset id and contract address
+
+  Options:\n#{NimbleOptions.docs(@interaction_functions)}
+  """
+  def get_functions(options) do
+    {:ok, params} = NimbleOptions.validate(options, @interaction_functions)
+
+    get!(
+      "#{@interactions}/#{params[:baseAssetId]}/contract_address/#{params[:contractAddress]}/functions"
+    )
+  end
+
+  @interaction_read [
+    baseAssetId: [type: :string, required: true, doc: "Base assetId e.g ETH, ETH_TEST5"],
+    contractAddress: [type: :string, required: true],
+    abiFunction: [type: :map]
+  ]
+  @doc """
+  Return deployed contract's ABI by blockchain native asset id and contract address
+
+  Options:\n#{NimbleOptions.docs(@interaction_read)}
+  """
+  def read(options, idempotentKey \\ "") do
+    {:ok, params} = NimbleOptions.validate(options, @interaction_read)
+    baseAssetId = params[:baseAssetId]
+    contractAddress = params[:contractAddress]
+
+    params =
+      params
+      |> Keyword.delete(:baseAssetId)
+      |> Keyword.delete(:contractAddress)
+      |> Jason.encode!()
+
+    post!(
+      "#{@interactions}/#{baseAssetId}/contract_address/#{contractAddress}/functions",
+      params,
+      idempotentKey
+    )
+  end
+
+  @interaction_write [
+    baseAssetId: [type: :string, required: true, doc: "Base assetId e.g ETH, ETH_TEST5"],
+    contractAddress: [type: :string, required: true],
+    vaultId: [
+      type: :string,
+      required: true,
+      doc: "The vault account id this contract was deploy from"
+    ],
+    abiFunction: [type: :map],
+    amount: [type: :string, doc: "Amount in base asset. Being used in payable functions"],
+    feeLevel: [
+      type: {:in, [:low, :medium, :high]},
+      default: :medium,
+      doc: "Fee level for the write function transaction. interchangeable with the 'fee' field"
+    ],
+    fee: [
+      type: :string,
+      doc:
+        "Max fee amount for the write function transaction. interchangeable with the 'feeLevel' field"
+    ],
+    note: [
+      type: :string,
+      doc:
+        "Custom note, not sent to the blockchain, that describes the transaction at your Fireblocks workspace"
+    ],
+    useGasless: [type: :boolean, default: false],
+    externalId: [
+      type: :string,
+      doc:
+        "External id that can be used to identify the transaction in your system. The unique identifier of the transaction outside of Fireblocks with max length of 255 characters"
+    ]
+  ]
+  @doc """
+  Call a write function on a deployed contract by blockchain native asset id and contract address. This creates an onchain transaction, thus it is an async operation. It returns a transaction id that can be polled for status check
+
+  Options:\n#{NimbleOptions.docs(@interaction_write)}
+  """
+  def write(options, idempotentKey \\ "") do
+    {:ok, params} = NimbleOptions.validate(options, @interaction_read)
+    baseAssetId = params[:baseAssetId]
+    contractAddress = params[:contractAddress]
+
+    params =
+      params
+      |> Keyword.delete(:baseAssetId)
+      |> Keyword.delete(:contractAddress)
+      |> Jason.encode!()
+
+    post!(
+      "#{@interactions}/#{baseAssetId}/contract_address/#{contractAddress}/functions",
+      params,
+      idempotentKey
+    )
+  end
+
+  @interaction_receipt [
+    baseAssetId: [type: :string, required: true, doc: "Base assetId e.g ETH, ETH_TEST5"],
+    txHash: [type: :string, required: true, doc: "Transaction hash"]
+  ]
+
+  @doc """
+  Retrieve the transaction receipt by blockchain native asset ID and transaction hash
+
+  Options:\n#{NimbleOptions.docs(@interaction_receipt)}
+  """
+  def get_transaction_receipt(receipt) do
+    {:ok, params} = NimbleOptions.validate(receipt, @interaction_receipt)
+
+    get!("#{@interactions}/#{params[:baseAssetId]}/tx_hash/#{params[:txHash]}/receipt")
+  end
+
+  @interaction_decode [
+    baseAssetId: [type: :string, required: true, doc: "Base assetId e.g ETH, ETH_TEST5"],
+    contractAddress: [type: :string, required: true],
+    dataType: [
+      type: {:in, [:error, :log, :function]},
+      doc: "Available values: `:error`, `:log`, `:function`"
+    ],
+    data: [
+      type: :map,
+      doc:
+        "The data to decode, which can be a string or an object containing the data and its type."
+    ],
+    abi: [type: {:list, :map}, doc: "The abi of the function/error/log to decode."]
+  ]
+
+  @doc """
+  Decode a function call data, error, or event log from a deployed contract by blockchain native asset id and contract address.
+
+  Options:\n#{NimbleOptions.docs(@interaction_decode)}
+  """
+  def decode(options, idempotentKey \\ "") do
+    {:ok, params} = NimbleOptions.validate(options, @interaction_decode)
+    baseAssetId = params[:baseAssetId]
+    contractAddress = params[:contractAddress]
+
+    params =
+      params
+      |> Keyword.delete(:baseAssetId)
+      |> Keyword.delete(:contractAddress)
+      |> Jason.encode!()
+
+    post!(
+      "#{@interactions}/#{baseAssetId}/contract_address/#{contractAddress}/decode",
+      params,
+      idempotentKey
+    )
   end
 end
