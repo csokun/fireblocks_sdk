d1e7087a5dc40e031646c96d169c255f42a8623e
sokun <chornsokun@gmail.com>
2026-07-07 12:22:21 +1000

    bump ver

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule FireblocksSdk.MixProject do
   use Mix.Project
 
-  @version "0.2.1"
+  @version "0.2.2"
   @repo_url "https://github.com/csokun/fireblocks_sdk"
 
   def getVersion(), do: @version
